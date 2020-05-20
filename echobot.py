b102b38e42a00b4bbc08de9bd86e91a5e471de6a
vvr3ddy <vvr-rockstar@bmsit.in>
2020-05-20 15:46:58 +0530

    :gear: bot : check if query was successful or not
    We must know if the bot fails in reading the query. To do so we check the JSON data received by the API Call and see if it results in a success or failure.
    If it is successful, proceed as usual. Else post an error message
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- echobot.py
+++ echobot.py
@@ -45,42 +45,56 @@ def start(update, context):
     update.message.reply_text('Hi 😊')
 
 def gNews(update, context):
-    """Send a message when the command /start is issued."""
-    id = update.message.chat_id
+    """Send a message when the command /gnews is issued."""
+
+    id = str(update.message.chat_id)
     news_api_key = os.environ.get("news_api_key","")
     url = "http://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey={}".format(news_api_key)
     response = requests.get(url)
     json_data = response.json()
+
     received_message = update.message.text
     splitMsg = received_message.split(" ", 1)
+    error_image = str("https://cheapdigitalservices.com/wp-content/uploads/error-with-wordpress.png")
     if len(splitMsg) == 1:
         splitMsg.append('0')
+    
     value = int(splitMsg[1])
     if value > 10  :
         value = 10
     elif value <= 0 :
         value = 10
-    update.message.reply_text('News from Google Feed\
-        powered by : NewsApi')
-    for count in range(value):
-        bot.send_chat_action(chat_id=id, action=telegram.ChatAction.TYPING)
-        # Get data from the JSON Response
-        author = json_data['articles'][count]['author']
-        title = json_data['articles'][count]['title']
-        newsUrl = json_data['articles'][count]['url']
-        url_string = str(newsUrl)
-        description = json_data['articles'][count]['description']
-        image = json_data['articles'][count]['urlToImage']
-        # If no author is assigned to the headline
-        if(author == None):
-            author = 'Not Announced'
 
-        # Finally spam the user with news 🌚
-        context.bot.send_photo(chat_id = str(id), photo = str(image), caption =
-                                f"\n<b>HeadLine :</b><i>{escape_html(title)}</i>"
-                                f"\n<b>Author   :</b><i>{escape_html(author)}</i>"
-                                f'\n<b>Source   :</b><a href ="{url_string}">Link</a>',
-                                parse_mode="HTML")  
+    if str(json_data['status']) == 'ok' :    
+        update.message.reply_text('News from Google Feed\
+            powered by : NewsApi')
+        for count in range(value):
+            # Get data from the JSON Response
+            author = json_data['articles'][count]['author']
+            title = json_data['articles'][count]['title']
+            newsUrl = json_data['articles'][count]['url']
+            url_string = str(newsUrl)
+            description = json_data['articles'][count]['description']
+            image = json_data['articles'][count]['urlToImage']
+            # If no author is assigned to the headline
+            if(author == None):
+                author = 'Not Announced'
+
+            # Finally spam the user with news 🌚
+            bot.send_chat_action(chat_id=id, action=telegram.ChatAction.TYPING)
+            context.bot.send_photo(chat_id = id, photo = str(image), caption =
+                                    f"\n<b>HeadLine :</b><i>{escape_html(title)}</i>"
+                                    f"\n<b>Author   :</b><i>{escape_html(author)}</i>"
+                                    f'\n<b>Source   :</b><a href ="{url_string}">Link</a>',
+                                    parse_mode="HTML")  
+    else :
+        bot.send_chat_action(chat_id=id, action=telegram.ChatAction.TYPING)
+        context.bot.send_photo(chat_id = id, photo = str(image), caption =
+                                    f"\n<b>The Bot has encountered some error.</b>"
+                                    f"\n<b>What can you do ?</b>"
+                                    f"\n<i>1. Retry the same command again.</i>"
+                                    f"\n<i>2. Notify the creator about the issue.</i>",
+                                    parse_mode="HTML")  
 
 
 
@@ -102,7 +116,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("gnews", gNews))
-    #dp.add.handler(CommandHandler("tnews", tNews))
+    #dp.add.handler(CommandHandler("tnews", tNews)) --> todo
     
 
     # log all errors
